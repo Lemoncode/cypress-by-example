43a7168e6580b38f641587b1896d3f3d83b94008
jaimesalas <jaime.salas@lemoncode.net>
2019-07-14 00:43:50 +0200

    added start projects

---
+++ 00_start/backend/server.js
@@ -0,0 +1,36 @@
+const cors = require('cors'), 
+    jsonServer = require('json-server'),
+    server = jsonServer.create(),
+    router = jsonServer.router('db.json'),
+    middlewares = jsonServer.defaults({
+        static: './public'
+    });
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(cors());
+
+server.delete('/api/hotels/all', async (_, res) => {
+    await router.db.setState({ hotels: [] });
+    res.sendStatus(200);
+});
+
+server.post('/api/hotels/bulkload', async ({ body: { hotels } }, res) => { 
+    await router.db.setState({ hotels });
+    res.sendStatus(200);
+});
+
+server.get('/api/cities', (req, res) => {
+    const cities = ['Seattle', 'Burlingame'];
+    res.send(cities);
+});
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1'
+}));
+
+server.use(router);
+
+const listener = server.listen(3000, () => {
+    console.log(`JSON Server is running at port ${listener.address().port}`);
+});
