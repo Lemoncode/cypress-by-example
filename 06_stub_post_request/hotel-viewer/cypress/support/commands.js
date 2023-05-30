30e079d634e8a5249ab641b697ea65e37bc6c69d
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-05-30 20:47:44 +0200

    removed migrated demos

--- 06_stub_post_request/hotel-viewer/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('loadAndVisit', (data = 'fixture:hotels') => {
-    cy.server();
-    cy.route('GET', 'http://localhost:3000/api/hotels', data)
-        .as('load');
-
-    cy.visit('/#/hotel-collection');
-
-    cy.wait('@load');
-}); 
