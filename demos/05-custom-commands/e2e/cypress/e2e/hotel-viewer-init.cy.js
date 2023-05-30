8e7dd77246cb69db7e50b31414967a0d712d74bd
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-05-30 17:18:25 +0200

    custom commands added

--- demos/05-custom-commands/e2e/cypress/e2e/hotel-viewer-init.cy.js
+++ demos/05-custom-commands/e2e/cypress/e2e/hotel-viewer-init.cy.js
@@ -2,9 +2,10 @@
 
 describe("Hotel viewer initialization", () => {
   it("displays hotels on page load", () => {
-    cy.visit("/hotel-collection");
-    cy.intercept("GET", "/api/hotels", { fixture: "hotels.json" }).as("load");
-    cy.wait('@load');
+    // cy.visit("/hotel-collection");
+    // cy.intercept("GET", "/api/hotels", { fixture: "hotels.json" }).as("load");
+    // cy.wait('@load');
+    cy.loadAndVisit();
     cy.get('[data-testid="hotels"] > li').should("have.length", 2);
   });
 });
--- demos/05-custom-commands/e2e/cypress/support/commands.js
+++ demos/05-custom-commands/e2e/cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loadAndVisit", (data = 'hotels.json') => {
+  cy.visit("/hotel-collection");
+  cy.intercept("GET", "/api/hotels", { fixture: data }).as("load");
+  cy.wait("@load");
+});
